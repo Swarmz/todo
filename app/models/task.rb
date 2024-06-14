eec2ee227dcca45be3d64131822429f9da636367
gavin <gavinsutton6511@gmail.com>
2024-06-14 13:06:13 +0900

    Add create task function, page and form

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,5 @@
 class Task < ApplicationRecord
+    validates :title, presence: true
+    validates :description, presence: true, length: { minimum: 6 }
+    validates :due_date, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "tasks#index"
-  get "/tasks", to: "tasks#index"
-  get "/tasks/:id", to: "tasks#show"
+  
+  resources :tasks
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
