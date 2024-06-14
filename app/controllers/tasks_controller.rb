bd5e36731d5774a04a6617dec4ce4ab30496c873
gavin <gavinsutton6511@gmail.com>
2024-06-14 13:28:55 +0900

    Add delete function

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,13 +12,34 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(title: "...", description: "...")
+    @task = Task.new(title: "...", description: "...", due_date: "...")
 
     if @task.save
       redirect_to @task
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+
+    if @task.update(task_params)
+      redirect_to @task
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    redirect_to root_path, status: :see_other
   end
 
   private
